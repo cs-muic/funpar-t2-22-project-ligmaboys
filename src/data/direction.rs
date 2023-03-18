4c6cc5b83fcca552e6ddfea72c6da3d6e89ee67d
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-03-19 01:51:48 +0700

    able to generate adjacency rules

--- src/data/direction.rs
+++ src/data/direction.rs
@@ -6,8 +6,19 @@ pub enum Direction {
     Right,
 }
 
+impl Direction {
+    pub fn to_idx(self) -> usize {
+        match self {
+            Direction::Up => 0,
+            Direction::Down => 2,
+            Direction::Left => 3,
+            Direction::Right => 1,
+        }
+    }
+}
+
 #[allow(dead_code)]
-const ALL_DIRECTIONS: [Direction; 4] = [
+pub const ALL_DIRECTIONS: [Direction; 4] = [
     Direction::Up,
     Direction::Down,
     Direction::Left,
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use clap::Parser;
 use cli::Args;
 
-use crate::data::direction::Direction;
+use crate::data::direction::ALL_DIRECTIONS;
 use crate::data::sample::{Sample, SampleID};
 
 extern crate image;
@@ -11,6 +11,8 @@ extern crate image;
 mod cli;
 mod data;
 mod image_reader;
+
+// TODO: Break down the logic into functions.
 
 fn main() {
     // Parse CLI <ImgPath> <Shape>
@@ -24,13 +26,11 @@ fn main() {
     image.load(&img);
 
     // Retrieve image samples (includes duplicates)
-    let samples = image.sample(args.n_dimensions as i32);
-
-    dbg!(&samples);
+    let unprocessed_samples = image.sample(args.n_dimensions as i32);
 
     // Calculate the number of times each unique sample appears
     let freq_map: HashMap<Sample, i32> =
-        samples
+        unprocessed_samples
             .iter()
             .fold(HashMap::<Sample, i32>::new(), |mut freq_map, sample| {
                 *freq_map.entry(sample.clone()).or_insert(0) += 1;
@@ -38,22 +38,54 @@ fn main() {
             });
 
     // Unzip the frequency map
-    let (_samples, freqs): (Vec<_>, Vec<_>) = freq_map.into_iter().unzip();
+    let (samples, freqs): (Vec<_>, Vec<_>) = freq_map.into_iter().unzip();
 
     // Assign each frequency to an ID
     // Note: The ID works w.r.t the sample vector
-    let freq_mapping: Vec<(SampleID, _)> = freqs
+    let _freq_mapping: Vec<(SampleID, _)> = freqs
         .iter()
         .enumerate()
         .map(|(i, freq)| (i as SampleID, freq))
         .collect();
 
-    dbg!(&freq_mapping);
+    // In the form [s1][direction][s2]
+    let mut adjacency_rules = vec![vec![vec![false; samples.len()]; 4]; samples.len()];
 
-    dbg!(&samples[0].compatible(&samples[1], Direction::Right));
-    dbg!(&samples[0].compatible(&samples[4], Direction::Down));
-    dbg!(&samples[5].compatible(&samples[4], Direction::Left));
-    dbg!(&samples[0].compatible(&samples[1], Direction::Left));
-    dbg!(&samples[0].compatible(&samples[1], Direction::Up));
-    dbg!(&samples[0].compatible(&samples[1], Direction::Down));
+    // Create adjacency rules
+    for s1 in 0..samples.len() {
+        for s2 in 0..samples.len() {
+            for direction in &ALL_DIRECTIONS {
+                if samples[s1].compatible(&samples[s2], *direction) {
+                    adjacency_rules[s1][direction.to_idx()][s2] = true;
+                }
+            }
+        }
+    }
+
+    // let sample_1 = samples
+    //     .clone()
+    //     .iter()
+    //     .position(|v| {
+    //         v.region
+    //             == vec![
+    //                 [0, 0, 0],
+    //                 [136, 136, 255],
+    //                 [0, 0, 0],
+    //                 [0, 0, 0],
+    //                 [136, 136, 255],
+    //                 [0, 0, 0],
+    //                 [136, 136, 255],
+    //                 [136, 136, 255],
+    //                 [136, 136, 255],
+    //             ]
+    //     })
+    //     .unwrap();
+
+    // let compatible: Vec<_> = (0..samples.len())
+    //     .into_iter()
+    //     .filter(|s_idx| *&adjacency_rules[sample_1][Direction::Down.to_idx()][*s_idx])
+    //     .collect();
+    // // dbg!(&compatible);
+    // dbg!(&samples[*&compatible[0]]);
+    // dbg!(&samples[sample_1]);
 }
